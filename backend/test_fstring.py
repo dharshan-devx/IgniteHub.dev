0008097769404bb6aafea2084a0e5fa833ca710a
Sondi Dharshan <dharshansondi.dev@gmail.com>
2026-06-19 22:41:58 +0530

    upgrade done for ideaforge++

--- backend/test_fstring.py
+++
@@ -1,7 +0,0 @@
-buildTime = '1 week'
-prompt = f"""{{ 
-  "title": "Creative and catchy project name",
-  "description": "Brief 1-2 sentence overview",
-  "estimated_time": "{buildTime}",
-}}"""
-print(prompt)
